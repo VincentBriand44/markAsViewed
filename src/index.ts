1a27cf950d694ab6a93ee62dc7b70bfad66eb1f0
VincentBriand44 <pro.vincent.briand@pm.me>
2024-10-05 15:32:29 +0200

    :sparkles: auto-addign custom url

--- src/index.ts
+++ src/index.ts
@@ -64,12 +64,21 @@ const goToEpisode = () => {
 	const list: NodeListOf<HTMLDivElement> =
 		document.querySelectorAll(".video-item-list");
 
-	if (list.length !== 1) return;
+	if (list.length === 1) {
+		const url: HTMLAnchorElement | null = list[0].querySelector(".top a");
+		const args = location.search.split("kaddon=")[1];
 
-	const url: HTMLAnchorElement | null = list[0].querySelector(".top a");
-	const args = location.search.split("kaddon=")[1];
+		window.location.href = `${url?.href}/${args}/?kaddon`;
+		return;
+	}
+	// biome-ignore lint/complexity/noForEach: <explanation>
+	list.forEach((item) => {
+		const anchor: HTMLAnchorElement | null = item.querySelector(".top a");
+		const args = location.search.split("kaddon=")[1];
 
-	window.location.href = `${url?.href}/${args}/?kaddon`;
+		if (!anchor?.href) return;
+		anchor.href = `${anchor.href}/${args}/?kaddon`;
+	});
 };
 
 (() => {
