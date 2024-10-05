01efcf57f4f456233227400c3d7903f654cd322e
VincentBriand44 <pro.vincent.briand@pm.me>
2024-10-05 14:08:05 +0200

    :ambulance: miss package.json import

--- build.cjs
+++ build.cjs
@@ -1,13 +1,17 @@
 const esbuild = require('esbuild');
-const {version} = require('package.json')
+const {version} = require('./package.json')
 
 const banner = `
 // ==UserScript==
-// @name         Your Script Name
+// @name         Mark as viewed
 // @namespace    http://tampermonkey.net/
 // @version      ${version}
-// @description  Your script description
-// @match        https://example.com/*
+// @description  Mark as viewed on AdKami from Crunchyroll
+// @author       Kanon
+// @match        https://www.crunchyroll.com/*
+// @match        https://www.adkami.com/video?search=*&kaddon=*
+// @match        https://www.adkami.com/anime*?kaddon*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=crunchyroll.com
 // @grant        none
 // ==/UserScript==
 `
