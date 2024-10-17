f7f79f0fc77f819c938cffe3ddbfa785617e462d
VincentBriand44 <pro.vincent.briand@pm.me>
2024-10-17 14:39:02 +0200

    :sparkles: auto-update feature

--- build.ts
+++ build.ts
@@ -9,6 +9,9 @@ const banner = `
 // @version      ${version}
 // @description  Mark as viewed on AdKami from Crunchyroll
 // @author       Kanon
+// @source       https://github.com/VincentBriand44/markAsViewed
+// @downloadURL  https://raw.githubusercontent.com/VincentBriand44/markAsViewed/refs/heads/main/dist/markAsViewed.user.js
+// @updateURL    https://raw.githubusercontent.com/VincentBriand44/markAsViewed/refs/heads/main/dist/markAsViewed.user.js
 // @match        http*://crunchyroll.com*
 // @match        http*://*.crunchyroll.com*
 // @match        http*://adkami.com/anime*?kaddon*
@@ -27,7 +30,7 @@ build({
 	minifyWhitespace: true,
 	sourcemap: false,
 	target: "esNext",
-	outfile: `dist/markAsViewed-v${version}.user.js`,
+	outfile: `dist/markAsViewed.user.js`,
 	banner: {
 		js: banner,
 	},
