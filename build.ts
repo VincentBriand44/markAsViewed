86d62f3632e39f5f06aa12c76498ca44c4c1fe99
VincentBriand44 <pro.vincent.briand@pm.me>
2024-10-17 14:38:00 +0200

    :bug: fix all match url

--- build.ts
+++ build.ts
@@ -9,9 +9,12 @@ const banner = `
 // @version      ${version}
 // @description  Mark as viewed on AdKami from Crunchyroll
 // @author       Kanon
-// @match        https://www.crunchyroll.com/*
-// @match        https://www.adkami.com/video?search=*&kaddon=*
-// @match        https://www.adkami.com/anime*?kaddon*
+// @match        http*://crunchyroll.com*
+// @match        http*://*.crunchyroll.com*
+// @match        http*://adkami.com/anime*?kaddon*
+// @match        http*://*.adkami.com/anime*?kaddon*
+// @match        http*://adkami.com/video?search=*&kaddon=*
+// @match        http*://*.adkami.com/video?search=*&kaddon=*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=crunchyroll.com
 // @grant        none
 // ==/UserScript==
