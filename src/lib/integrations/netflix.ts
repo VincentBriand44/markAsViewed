14dee0ef963f60df678c3663cbb02d0e9260b01b
VincentBriand44 <pro.vincent.briand@pm.me>
2024-10-21 18:46:18 +0200

    :bug: fix season netflix

--- src/lib/integrations/netflix.ts
+++ src/lib/integrations/netflix.ts
@@ -5,12 +5,23 @@ const integration = (): IntegrationData => {
 		'div[data-uia="video-title"]',
 	);
 	const episodeElement = Number(
-		titleElement?.querySelectorAll("span")[0]?.textContent?.split("E")[0],
+		titleElement?.querySelectorAll("span")[0]?.textContent?.split("E")[1],
 	);
+	const seasonElement = document
+		.querySelector(
+			'div[data-uia="pause-ad-title-display-info-container"] > div > span',
+		)
+		?.innerHTML.split("&nbsp;:&nbsp;")[0];
+	console.log("🚀 ~ integration ~ seasonElement:", seasonElement);
 
 	const title = titleElement?.querySelector("h4")?.textContent;
+	console.log("🚀 ~ integration ~ title:", title);
 	const episode = episodeElement ?? 1;
-	const season = Number(titleElement?.querySelector("h4")?.textContent ?? 1);
+	console.log("🚀 ~ integration ~ episode:", episode);
+	const season = Number(
+		seasonElement?.startsWith("S") ? seasonElement.split("S")[1] : 1,
+	);
+	console.log("🚀 ~ integration ~ season:", season);
 
 	if (!title || !season) throw new Error("data not found");
 
