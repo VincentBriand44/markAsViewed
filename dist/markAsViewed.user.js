7fcd732f7f89209474ea0bb01384d6a3058f4b77
VincentBriand44 <pro.vincent.briand@pm.me>
2024-11-16 20:47:28 +0100

    :rocket: build

--- dist/markAsViewed.user.js
+++ dist/markAsViewed.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name         Mark as viewed
 // @namespace    http://tampermonkey.net/
-// @version      1.2.3
+// @version      1.3.0
 // @description  Mark as viewed on AdKami from Crunchyroll
 // @author       Kanon
 // @source       https://github.com/VincentBriand44/markAsViewed
