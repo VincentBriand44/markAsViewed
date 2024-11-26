b4426bfd313a58aa569d70ad880daf03688a437d
VincentBriand44 <pro.vincent.briand@pm.me>
2024-11-26 14:57:12 +0100

    :sparkles: goto if multi-result in search

--- src/lib/goToEpisode.ts
+++ src/lib/goToEpisode.ts
@@ -2,13 +2,23 @@ const goToEpisode = () => {
 	const list = document.querySelectorAll<HTMLDivElement>(".video-item-list");
 	const args = location.search.split("kaddon=")[1];
 
-	if (list.length !== 1) return;
+	if (list.length > 1) {
+		for (const item of Array.from(list)) {
+			const anchor = item.querySelector<HTMLAnchorElement>(".top a");
 
-	const anchor = list[0].querySelector<HTMLAnchorElement>(".top a");
-	if (anchor?.href) {
-		window.location.href = `${anchor.href}/${args}/?kaddon`;
+			if (anchor?.href) {
+				anchor.href = `${anchor.href}/${args}/?kaddon`;
+			}
+		}
+	} else {
+		const anchor = list[0].querySelector<HTMLAnchorElement>(".top a");
+
+		if (anchor?.href) {
+			window.location.href = `${anchor.href}/${args}/?kaddon`;
+		}
+
+		return;
 	}
-	return;
 };
 
 export default goToEpisode;
